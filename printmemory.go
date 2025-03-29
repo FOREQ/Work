46ca75db6b5bf0b862be873e4ed5ead024fac749
Yebaibakty <hfrozen59@gmail.com>
2025-03-29 10:47:21 +0500

    fdfd

---
+++ printmemory.go
@@ -0,0 +1,23 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintMemory(arr [10]byte) {
+	hex := "0123456789abcdef"
+	for i := 0; i < len(arr); i++ {
+		z01.PrintRune(rune(hex[arr[i]>>4]))
+		z01.PrintRune(rune(hex[arr[i]&15]))
+		z01.PrintRune(' ')
+		if (i-3)%4 == 0 || i == len(arr)-1 {
+			z01.PrintRune('\n')
+		}
+	}
+	for _, b := range arr {
+		if b < 32 || b > 128 {
+			z01.PrintRune('.')
+		} else {
+			z01.PrintRune(rune(b))
+		}
+	}
+	z01.PrintRune('\n')
+}
